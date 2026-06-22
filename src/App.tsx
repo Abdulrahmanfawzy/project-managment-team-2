4b774001e6a49666dfe8e03d198b8fb45d4cd280
Ahmed Fouad Abodeaf <100656655+ahmed7-fouad@users.noreply.github.com>
2026-06-22 23:57:55 +0300

    feat: refactor navigation structure and integrate AuthPagesNav in Login component

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import LangSwitcher from "./components/shared/lang-switcher";
 
 // Main Components
 import Login from "./components/Login";
-import Nav from "./components/Nav";
 
 export default function App() {
   const { t } = useTranslation();
 
   return (
     <>
-      <Nav/>
       <Login />
     </>
   );

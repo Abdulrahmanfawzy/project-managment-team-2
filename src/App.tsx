0e0eb04bd9cd1858c58a11f832e4eb8d8b084d29
Ahmed Fouad Abodeaf <100656655+ahmed7-fouad@users.noreply.github.com>
2026-06-25 01:30:19 +0300

    feat: implement ProfilePage with navigation and analysis cards; add ProfilePageAbout and AnalysisCard components

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { useTranslation } from "react-i18next";
 import LangSwitcher from "./components/shared/lang-switcher";
 
 // Main Components
+import AuthPageNav from "./components/AuthPagesNav";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import ResetPassword from "./components/ResetPassword";
+import ProfilePage from "./components/profilePage";
+
 export default function App() {
   const { t } = useTranslation();
 
   return (
     <>
+      <ProfilePage />
+      {/* <ResetPassword/> */}
     </>
   );
 }
--- src/components/shared/lang-switcher.tsx
+++ src/components/shared/lang-switcher.tsx
@@ -14,12 +14,12 @@ export default  function LangSwitcher() {
       <SelectTrigger className="max-w-48 p-6 px-4 rounded-2xl cursor-pointer text-md! font-semibold!  data-[state=open]:text-md!   data-[state=open]:font-semibold! data-[placeholder]:text-md! data-[placeholder]:font-semibold! [&_svg]:text-mainText! [&_svg]:w-4!">
         <SelectValue placeholder="English" />
       </SelectTrigger>
-      <SelectContent>
+      <SelectContent className="relative z-150">
         <SelectGroup>
-          <SelectItem value="grapes" className="capitalize">
+          <SelectItem value="grapes" className="capitalize cursor-pointer">
             English
           </SelectItem>
-          <SelectItem value="pineapple" className="capitalize">
+          <SelectItem value="pineapple" className="capitalize cursor-pointer">
             Arabic
           </SelectItem>
         </SelectGroup>
